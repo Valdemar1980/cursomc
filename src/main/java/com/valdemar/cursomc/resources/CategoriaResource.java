3799f1fb3bc6e4ea453a73ef3a32ca453c8bbb6c
Valdemar1980 <valdemar.java@gmail.com>
2022-04-11 19:25:27 -0300

    Listando todas as categoriascom DTO

--- src/main/java/com/valdemar/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/valdemar/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package com.valdemar.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.valdemar.cursomc.domain.Categoria;
+import com.valdemar.cursomc.domain.dto.CategoriaDTO;
 import com.valdemar.cursomc.services.CategoriaService;
 
 @RestController
@@ -51,6 +54,14 @@ public class CategoriaResource {
 		service.delete(id);
 		
 		return ResponseEntity.noContent().build(); //conteudo  vazio
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
 		
+		List<CategoriaDTO> listDto =  list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok(listDto);
 	}
 }
