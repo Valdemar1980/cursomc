ea9aa87e4294ba0b6b1dd3f91fd71618198d6fb4
Valdemar1980 <valdemar.java@gmail.com>
2022-05-02 17:49:03 -0300

    Validacao sintatica com bean Validation

--- src/main/java/com/valdemar/cursomc/domain/dto/CategoriaDTO.java
+++ src/main/java/com/valdemar/cursomc/domain/dto/CategoriaDTO.java
@@ -2,13 +2,23 @@ package com.valdemar.cursomc.domain.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+
 import com.valdemar.cursomc.domain.Categoria;
+
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento Obrigatorio em branco")
+	@NotNull(message="Preenchimento Obrigatorio null")
+	@Length(min=5,max=80,message="O tamanho deve ser entre 5 e 80 caracteres.")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/valdemar/cursomc/services/CategoriaService.java
+++ src/main/java/com/valdemar/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.valdemar.cursomc.domain.Categoria;
+import com.valdemar.cursomc.domain.dto.CategoriaDTO;
 import com.valdemar.cursomc.repositories.CategoriaRepository;
 import com.valdemar.cursomc.services.exceptions.DataIntegrityException;
 import com.valdemar.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage,  Direction.valueOf(direction),  orderBy);
 		
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria  fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
